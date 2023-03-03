cc31f104f934859fc9b723b4036a98021a54fd18
Taaran18 <taaranjain16@gmail.com>
2023-03-03 16:43:41 +0530

    update

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -2,18 +2,22 @@ function getBotResponse(input) {
     //rock paper scissors
     if (input == "rock") {
         return "paper";
-    } else if (input == "paper") {
+    } 
+    else if (input == "paper") {
         return "scissors";
-    } else if (input == "scissors") {
+    } 
+    else if (input == "scissors") {
         return "rock";
     }
 
     // Simple responses
     if (input == "hello" || input == "hi") {
         return "Hello there!";
-    } else if (input == "goodbye") {
+    } 
+    else if (input == "goodbye") {
         return "Talk to you later!";
-    } else {
+    } 
+    else {
         return "Try asking something else!";
     }
 }
